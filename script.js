65ae6759ab6d2836235a639a8d1fbde68cea6b19
Ricomar1 <stuuhgo@gmail.com>
2026-01-31 21:48:51 +0100

    Remove welcome modal and contact form functions
    Removed welcome modal functionality and contact form alert.

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById("welcomeModal");
-  modal.showModal();
-
-  window.addEventListener("click", (e) => {
-    if (!modal.contains(e.target) && modal.open) {
-      modal.close();
-    }
-  });
-});
-
-function closeModal() {
-  const modal = document.getElementById("welcomeModal");
-  modal.close();
-}
-
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
   } else {
     document.exitFullscreen();
   }
-}
-
-function openContactForm() {
-  alert("Module de contact à venir !");
 }
 
 document.getElementById("toggleTheme").addEventListener("click", () => {
